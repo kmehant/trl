a3eb925b6de9f466bd402741be35eaba967bcc0c
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-09-01 11:59:28 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- trl/trainer/sft_trainer.py
+++ trl/trainer/sft_trainer.py
@@ -595,7 +595,7 @@ class SFTTrainer(Trainer):
             model = get_peft_model(model, peft_config)
         print("#####")
         for n, p in model.named_parameters():
-            print(n, p.dtype)
+            print(f"{n}->{p.dtype}")
         print("#####")
         # Handle bf16 casting for 4-bit models
         if args.bf16 and getattr(model, "is_loaded_in_4bit", False) and not is_sharded_qlora:
